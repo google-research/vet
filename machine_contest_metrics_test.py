b4d1f8e69121b7ed1bd676741cf30e8a136da969
shubin <shubin@google.com>
2024-02-29 15:24:18 -0500

    Use the absl parameterized test to avoid google3 dependency.
    PiperOrigin-RevId: 611557775

--- machine_contest_metrics_test.py
+++ machine_contest_metrics_test.py
@@ -17,9 +17,8 @@ Tests machine_contest_metrics.
 
 from typing import Any, Callable
 
-from google3.testing.pybase import parameterized
-
 from absl.testing import absltest
+from absl.testing import parameterized
 import numpy as np
 import datatypes
 import machine_contest_metrics
