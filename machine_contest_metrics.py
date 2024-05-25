Corrects p-value calculation to properly take into account ties between the scores of the null vs alternative hypothesis. This is based on the definition that the p-value is the probability that a test statistic obtained from the hypothesis hypothesis is at least as extreme as the observed test statistic (from the alternative hypothesis). Both "directions" for calculating p-value, namely f(A,G)-f(B,G) and f(B,G)-f(A,G), are considered and the smaller of these is chosen. Note that, due to the possibility of ties, the p-value need not be less than or equal to 0.5 and, hence, cannot be doubled to obtain a two-sided p-value.
PiperOrigin-RevId: 637246183

machine_contest_metrics.py
@@ -575,38 +575,40 @@ def calculate_p_value(
   Returns:
     The p-value for the results.
   """
-  # Preprocessing for two-sided hypothesis test (ie, M1 > M2 OR M2 > M1).
+  # Preprocessing for two-sided hypothesis test (ie, M1 >> M2 OR M2 >> M1):
+  # use the side having fewer scores in s_null > s_alt (hence, smaller p-value).
   if two_sided_test and (np.median(s_null) > np.median(s_alt)):
     s_null, s_alt = -s_null, -s_alt
 
   s_null = sorted(s_null, reverse=True)
   s_alt = sorted(s_alt, reverse=True)
 
-  # If all null hypothesis results are smaller than all alternative hypothesis
-  # results then the p-value is 0 and, hence, H_0 is rejected.
-  if s_null[0] < s_alt[-1]:
-    return 0.0
-  # If all null hypothesis results are larger than all alternative hypothesis
-  # results then the p-value is 1 and, hence, H_0 clearly cannot be rejected.
-  elif s_alt[0] < s_null[-1]:
+  # If all null hypothesis scores are as large as all alternative hypothesis
+  # scores then the p-value is 1 and, hence, H_0 is rejected.
+  if s_null[-1] >= s_alt[0]:
     return 1.0
+  # If all alt hypothesis scores are greater than all null hypothesis scores
+  # then the p-value is 0 and, hence, H_0 clearly cannot be rejected.
+  elif s_alt[-1] > s_null[0]:
+    return 0.0
 
+  # For each s_null value, count all s_alt values that are smaller or equal.
   p_count = 0  # numerator of p-value
   i = j = 0
   while i < len(s_null) and j < len(s_alt):
-    if s_null[i] >= s_alt[j]:
-      i += 1
-    else:
-      p_count += i
+    if s_null[i] < s_alt[j]:
+      # Null test statistic not as large as alt so try smaller alt score.
       j += 1
+    else:
+      # Null test statistic is at least as large as alt so count entire suffix.
+      p_count += len(s_alt) - j
+      i += 1
 
-  # There are more s_alt items bigger than the last one in s_null.
-  # They should all be counted.
-  if j < len(s_alt) - 1 and s_alt[j] > s_null[-1]:
-    p_count += (len(s_alt) - j - 1) * (len(s_null) - 1)
+  # If i == len(s_null) then there are no more null values to count for.
+  # If j == len(s_alt) then there are no alt values left to count.
 
   p_value = p_count / (len(s_null) * len(s_alt))
-  return 2 * p_value if two_sided_test else p_value
+  return p_value
 
 def mean_and_confidence_bounds(
     scores: np.ndarray,

machine_contest_metrics_test.py
@@ -340,7 +340,7 @@ class MachineContestMetricsTest(absltest.TestCase):
     s_null = np.random.permutation(10)  # order doesn't matter
     s_alt = np.repeat(4.5, 10)
     p_value = machine_contest_metrics.calculate_p_value(s_null, s_alt)
-    self.assertAlmostEqual(p_value, 1.0, places=2)
+    self.assertAlmostEqual(p_value, 0.5, places=2)
 
   def test_calculate_p_value_null_all_greater(self):
     """Test the case where `s_alt` is no better than `s_null` for all scores."""
@@ -359,11 +359,44 @@ class MachineContestMetricsTest(absltest.TestCase):
     self.assertAlmostEqual(p_value, 0.0, places=2)
 
   def test_calculate_p_value_same_items(self):
-    """Test when `s_null` and `s_alt` have the same items."""
+    """Test when `s_null` and `s_alt` have the same set of (unequal) scores."""
     s_null = np.random.permutation(10)
     s_alt = np.random.permutation(10)
     p_value = machine_contest_metrics.calculate_p_value(s_null, s_alt)
-    self.assertAlmostEqual(p_value, 0.9, places=2)
+    self.assertAlmostEqual(p_value, 0.55, places=2)
+
+  def test_calculate_p_value_same_items_all_equal(self):
+    """Test when `s_null` and `s_alt` have equal-valued items."""
+    s_null = np.repeat(0.5, 10)
+    s_alt = np.repeat(0.5, 10)
+    p_value = machine_contest_metrics.calculate_p_value(s_null, s_alt)
+    self.assertAlmostEqual(p_value, 1.0, places=2)
+
+  def test_calculate_p_value_with_ties(self):
+    s_null = np.concatenate([np.zeros(50), np.ones(50)])
+    s_alt = np.concatenate([np.zeros(60), np.ones(40)])
+    p_value1 = machine_contest_metrics.calculate_p_value(
+        s_null, s_alt, two_sided_test=False)
+    p_value2 = machine_contest_metrics.calculate_p_value(
+        s_null, s_alt, two_sided_test=True)
+    self.assertAlmostEqual(p_value1, 0.8, places=2)
+    self.assertAlmostEqual(p_value2, 0.7, places=2)
+
+  def test_calculate_p_value_null_outlier(self):
+    s_null = np.asarray([100, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+    s_alt = np.repeat(10, 10)
+    p_value = machine_contest_metrics.calculate_p_value(s_null, s_alt)
+    self.assertAlmostEqual(p_value, 0.1, places=2)
+
+  def test_calculate_p_value_alt_outlier(self):
+    s_null = np.repeat(10, 10)
+    s_alt = np.asarray([100, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+    p_value1 = machine_contest_metrics.calculate_p_value(
+        s_null, s_alt, two_sided_test=False)
+    p_value2 = machine_contest_metrics.calculate_p_value(
+        s_null, s_alt, two_sided_test=True)
+    self.assertAlmostEqual(p_value1, 0.9, places=2)
+    self.assertAlmostEqual(p_value2, 0.1, places=2)
 
   def test_mean_confidence_bounds(self):
     scores = np.arange(1000)